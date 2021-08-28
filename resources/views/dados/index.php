27f9b8958e8d84a12c6e4ade2418ff02b1d8387b
Weslley Bezerra <weslleybezerra95@gmail.com>
2021-08-28 15:21:21 -0300

    alter struct html

--- resources/views/dados/index.php
+++ resources/views/dados/index.php
@@ -5,20 +5,30 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
-  <!-- CSS Bootstratp -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-  
+
   <title>Dada Controller</title>
+  <!-- CSS Bootstratp -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 
 <body>
-  <center><b>Corinthians</b></center>
-  <ul class="list-group">
-      <?php    foreach ($dados as $dado) : ?>
-      <li><center><?= $dado; ?></center></li>
-      <?php    endforeach; ?>
-  </ul>
+    
+  <div class="container">
+    <div class="jumbotron">
+      <h1 class="display-6">Dados</h1>
+    </div>
+      
+      <a href="#" class="btn btn-dark m-3">Adicionar</a>
+      
+
+    <ul class="list-group">
+      <?php foreach ($dados as $dado) : ?>
+        <li class="list-group-item">
+          <center><?= $dado; ?></center>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
 </body>
 
 </html>
